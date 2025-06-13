a66084cfd707e49f3c904d8fe8c23373e55e35ff
abdelali221 <abdelalibouyahiaoui200@gmail.com>
2025-06-13 13:46:46 +0100

    Update UI.c

--- source/UI.c
+++ source/UI.c
@@ -253,8 +253,7 @@ void donut() {
 	printf("Credits : Andy Sloane (AIKON), Original donut code author.");
 	POSCursor(17, 26);
 	printf("Akhilesh Thile, donut.c author.");
-    for(;;) {
-		memset(b,32,1760);
+     memset(b,32,1760);
         memset(z,0,7040);
         for(j=0; j < 6.28; j += 0.07) {
             for(i=0; i < 6.28; i += 0.02) {
@@ -270,17 +269,17 @@ void donut() {
                 float n = sin(B);
                 float t = c * h * g - f * e;
                 int x = 40 + 30 * D * (l * h * m - t * n);
-                int y= 10 + 15 * D * (l * h * n + t * m);
+                int y= 12 + 15 * D * (l * h * n + t * m);
                 int o = x + 80 * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if(20 > y && y > 0 && x > 0 && 60 > x && D > z[o]) {
+                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
                     z[o] = D;
                     b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 }
             }
         }
         printf("\x1b[H");
-        for(k = 0; k < 1500; k++) {
+        for(k = 0; k < 1761; k++) {
             putchar(k % 80 ? b[k] : 10);
             A += 0.00004;
             B += 0.00002;
